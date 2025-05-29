96ab7538abf6bceab32c1b21fc6cca96ce8e484f
wiamerz <wiamerm2023@gmail.com>
2025-05-29 12:45:53 +0100

    Orchestration avec Docker Compose

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+// Use your env vars
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
 });
@@ -10,3 +22,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connecté'))
+  .catch(err => console.error(err));
+
